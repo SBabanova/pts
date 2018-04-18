226564bc19f2a4d6cb10982fad375b60f562c8b6
SBabanova <38456432+SBabanova@users.noreply.github.com>
2018-04-18 16:14:59 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/tp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp.dir/main.cpp.obj"
+  "tp.pdb"
+  "tp.exe"
+  "tp.exe.manifest"
+  "libtp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
